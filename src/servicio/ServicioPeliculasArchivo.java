f3c5e09fb82bb9e87bf105d1ed083a7fe757e291
Santiago Rivera <88156315+Hacktiago@users.noreply.github.com>
2023-08-16 16:48:10 -0600

    Update ServicioPeliculasArchivo.java

--- src/servicio/ServicioPeliculasArchivo.java
+++ src/servicio/ServicioPeliculasArchivo.java
@@ -62,10 +62,8 @@ public class ServicioPeliculasArchivo implements IServicioPeliculas{
       // Agregamos la pelicula (toString)
       salida.println(pelicula);
       salida.close();
-      System.out.println("Se agrego al archivo la pelicula: " + pelicula.getNombre());
       return pelicula;
     } catch(Exception e){
-      System.out.println("Ocurrio un error al agregar pelicula: " + e.getMessage());
       return null;
     }
   }
